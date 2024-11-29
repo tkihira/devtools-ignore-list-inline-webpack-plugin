59aa7ec884eef6d8e32eb9af5d22d399a337c832
Takuo Kihira <takuo.kihira@gmail.com>
2024-11-29 19:18:19 +0900

    initial commit

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import { Compilation } from 'webpack';
+
+class DevToolsIgnoreListInlinePlugin {
+
+    constructor(options) {
+        this.options = {
+            isTarget: options?.isTarget ?? (filename => filename.endsWith('.js')),
+        }
+    }
+
+    apply(compiler) {
+        compiler.hooks.compilation.tap('DevToolsIgnoreListInlinePlugin', (compilation) => {
+            compilation.hooks.processAssets.tap(
+                {
+                    name: 'DevToolsIgnoreListInlinePlugin',
+                    stage: Compilation.PROCESS_ASSETS_STAGE_DEV_TOOLING,
+                },
+                (assets) => {
+                    for (const [filename, asset] of Object.entries(assets)) {
+                        const fullPath = path.resolve(compilation.outputOptions.path, filename);
+                        if (this.options.isTarget(filename)) {
+                            const code = asset.source().toString();;
+                            const sourceMapContent = `{"version":3,"file":"${filename}","mappings":"AAAA","sources":["${filename}"],"sourcesContent":[""],"ignoreList":[0]}`;
+                            const base64SourceMapContent = Buffer.from(sourceMapContent).toString("base64");
+                            const inlineSourceMapComment = `\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,${base64SourceMapContent}`;
+
+                            compilation.updateAsset(
+                                filename,
+                                new compiler.webpack.sources.RawSource(code + inlineSourceMapComment)
+                            );
+                        }
+                    }
+                }
+            );
+        });
+    }
+}
+
+// hack to support commonjs
+DevToolsIgnoreListInlinePlugin.default = DevToolsIgnoreListInlinePlugin;
+
+export default DevToolsIgnoreListInlinePlugin;
